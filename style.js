c5191472b5d68f43758df8a9d03458da199c8936
Vitaliy <Mushpakov.Vitaliy@gmail.com>
2021-04-03 20:26:50 +0300

    fix

--- style.js
+++ style.js
@@ -1,55 +1,50 @@
 // _______________№_1_________________
-// let arr = [];
-// let arr1 = [];
+let arr = [];
+let arr1 = [];
+let max = 0;
 
-// for(i = 0; i < 10; i++) {
-//     arr[i] = Math.round(Math.random(i)*100)
-//     arr1[i] = Math.round(Math.random(i)*100)
-// }
-// console.log(arr, arr1);
+for(i = 0; i < 10; i++) {
+    arr[i] = Math.round(Math.random(i)*100)
+    arr1[i] = Math.round(Math.random(i)*100)
+}
+console.log(arr, arr1);
 
 // _____________________№_2__________________
 
 
-// let max = 0;
-// let min;
-// function con() {
-//   let arr = [];
-//   let arr1 = [];
-//   let arr2 = arr.concat(arr1);
-//   arr2.push(22, 13, 50, 33, 99, 68, 5, 10);
-//   console.log(arr2);
+function con(array_1, array_2) {
+  let arr2 = arr.concat(arr1);
+  arr2.push(22);
+  console.log(arr2);
+  let min = arr2[0];
+    for (let j = 0; j < arr2.length; j++){
+        if(arr2[j] > max){
+            max = arr2[j]
+        }
+    }
 
-//     for (let j = 0; j < arr2.length; j++){
-//         if(arr2[j] > max){
-//             max = arr2[j]
-//         }
-//     }
-
-//     for (let k = 0; k < arr2.length; k++){
-//       if(arr2[k] < max){
-//           min = arr2[k]
-//       }
-//   }
+    for (let k = 0; k < arr2.length; k++){
+      if(arr2[k] < min){
+          min = arr2[k]
+      }
+  }
     
-//     for (let i = 0; i < arr2.length; i++) {
-//       if(arr2[i] % 2 == 0 ){
-//         let arr3 = arr2[i]*2;
-//         console.log(arr3);
-//       } 
-//     }       
-// }
-// con();
-// console.log(`maxNumber: ${max}`);  
-// console.log(`minNumber: ${min}`);
-// // почему-то выводит неправильное значение!!!
+    for (let i = 0; i < arr2.length; i++) {
+      if(arr2[i] % 2 == 0 ){
+        let arr3 = arr2[i]*2;
+        console.log(arr3);
+      } 
+    }  
+    console.log(`minNumber: ${min}`); 
+    console.log(`maxNumber: ${max}`);     
+}
+
+// почему-то выводит неправильное значение!!!
 
 // ________________№3__________________
 
-// function arrows(num, num2) {
-//   console.log(num, num2);
-// }
 
-// arrows(arr, arr1)
+
+con(arr, arr1)
 
 
